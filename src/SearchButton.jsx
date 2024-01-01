55278312ad8ae582a8484faae390e58c58077549
Youbraj Singh <blazeplays2003@gmail.com>
2024-01-02 00:38:41 +0530

    added the get data functionality

--- src/SearchButton.jsx
+++ src/SearchButton.jsx
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import "./SearchButton.css"
+import "./SearchButton.css";
+import {getData} from "./getWeatherData.js"
 
 export const SearchButton = () => {
-    let [city,setCity] = useState("");
+  let [city, setCity] = useState("");
 
-    let handleChange = (e)=>{
-        setCity(e.target.value);
-    };
+  let handleChange = (e) => {
+    setCity(e.target.value);
+  };
 
-    let handleSubmit = (e)=>{
-        e.preventDefault();
-        console.log(city);
-        setCity("")
-    }
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(city);
+    getData(city);
+    setCity("");
+  };
+ 
 
   return (
-    <div className="SearchButton" >
+    <div className="SearchButton">
       <form onSubmit={handleSubmit}>
         <h2>Weather App</h2>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" value={city} onChange={handleChange} />
-        <br /><br />
-        <Button variant="contained" type="submit">Seacrh</Button>
+        <TextField
+          id="outlined-basic"
+          label="Enter City name"
+          variant="outlined"
+          value={city}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <Button variant="contained" type="submit">
+          Seacrh
+        </Button>
       </form>
     </div>
   );
---
+++ src/getWeatherData.js
@@ -0,0 +1,19 @@
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+const apiUrl = import.meta.env.VITE_WEATHER_API_URL; 
+
+export let getData = async (city)=>{
+    let res = await fetch(`${apiUrl}q=${city}&appid=${apiKey}&units=metric`);
+    let jres = await res.json();
+    let result = {
+        temp:jres.main.temp,
+        humidity:jres.main.humidity,
+        feels_like:jres.main.feels_like,
+        temp_min:jres.main.temp_min,
+        temp_max:jres.main.temp_max,
+        pressure:jres.main.pressure,
+        weather:jres.weather[0].description
+        
+    }
+    console.log(result)
+}
+ 
